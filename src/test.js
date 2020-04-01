c4d22fe44f461691c4e40ecdf599afce53cc6ac9
Santosh Marigowda <santosharakere@gmail.com>
2020-04-01 10:42:01 +0100

    added a new command, product to math service

--- src/test.js
+++ src/test.js
@@ -5,6 +5,11 @@ seneca.add({ service: "math", command: "sum" }, (message, reply) => {
   reply(null, { answer: left + right });
 });
 
+seneca.add({ service: "math", command: "product" }, (message, reply) => {
+  let { left, right } = message.data;
+  reply(null, { answer: left * right });
+});
+
 seneca.act(
   { service: "math", command: "sum", data: { left: 1, right: 2 } },
   (err, result) => {
@@ -12,7 +17,15 @@ seneca.act(
       console.log(err);
       throw err;
     }
-    console.log('Service responded with result ! SUCCESS...')
+    console.log("Service responded with result ! SUCCESS...");
     console.log(result);
   }
 );
+
+seneca.act({ service: 'math', command: 'product', data: { left: 2, right: 5 }}, (err, result) => {
+  if(err) {
+    console.log('Error while serving command - product');
+    throw err;
+  }
+  console.log(result);
+})
