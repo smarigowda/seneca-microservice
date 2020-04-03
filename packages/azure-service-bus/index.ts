4bf0502ec82e827e6a588413423a4284c0a2f0df
Santosh Marigowda <santosharakere@gmail.com>
2020-04-03 11:45:07 +0100

    function with default parameter

--- packages/azure-service-bus/index.ts
+++ packages/azure-service-bus/index.ts
@@ -1,13 +1,26 @@
 require("source-map-support").install();
 
-class Foo {
-  constructor() {
-    this.bar();
-  }
+// class Foo {
+//   constructor() {
+//     this.bar();
+//   }
 
-  bar() {
-    throw new Error("This is a demo...");
-  }
+//   bar() {
+//     throw new Error("This is a demo...");
+//   }
+// }
+
+// new Foo();
+
+type Context = {
+  appId?: string;
+  userId?: string;
+};
+
+function log(message: string, context: Context = {}) {
+  let time = new Date().toISOString();
+  console.log(time, message, context.userId);
 }
 
-new Foo();
+log("Hello");
+log("Hello", { userId: "1234" });
